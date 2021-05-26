47bcf489f94dfeaf5006e1f9ddfc184260c0bf46
somanath94 <hm.somu67@gmail.com>
2021-05-26 22:41:41 +0530

    updated in master branch

--- DButils.java
+++ DButils.java
@@ -1,3 +1,4 @@
+Hello guys I am updating the code file
 Hello Guys......
 GM/GA/GE
 git commands are very important do more practice
--- users.java
+++ users.java
@@ -1,1 +1,2 @@
+Hello I am in master branch
 Hello I am devloping in dev branch	
